264f227613b65cd251714b1c2c25e15649ac027b
Tushar Diwakar <44468541+Tusharsd123@users.noreply.github.com>
2021-02-14 18:14:17 +0530

    Time: 20 ms (84.31%), Space: 15.7 MB (42.88%) - LeetHub

--- shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
+++ shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
         vector<vector<pair<int,int>>> g(n);
-        for(auto &v : red_edges)
+        for(auto v : red_edges)
             g[v[0]].push_back({v[1],0});
-        for(auto &u : blue_edges)
+        for(auto u : blue_edges)
             g[u[0]].push_back({u[1],1});
         vector<vector<int>> vCost(n,vector<int>(2,-1));
         queue<pair<int,int>> q;
@@ -15,7 +15,7 @@ public:
             {
                 pair<int,int> p = q.front();
                 q.pop();
-                for(auto &v : g[p.first])
+                for(auto v : g[p.first])
                 {
                     if(v.second==p.second || vCost[v.first][v.second]!=-1)
                         continue;
@@ -24,7 +24,7 @@ public:
                 }
             }
         vector<int> res;
-        for(auto &v : vCost)
+        for(auto v : vCost)
             {
                 sort(v.begin(),v.end());
                 res.push_back(v[0]!=-1?v[0]:v[1]);
