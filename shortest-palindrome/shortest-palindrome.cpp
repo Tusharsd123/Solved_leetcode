dad6344f9dbaf55707c534670230bfc932375ac8
Tushar Diwakar <44468541+Tusharsd123@users.noreply.github.com>
2021-02-17 12:36:14 +0530

    Time: 4 ms (84.88%), Space: 8 MB (38.78%) - LeetHub

---
+++ shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+          string rev_s = s;
+          reverse(rev_s.begin(),rev_s.end());
+          string l = s + '#' + rev_s; //for no overlap
+          vector<int> p(l.size(),0);
+          for(int i = 1;i<l.length();i++)
+          {
+              int j = p[i-1];
+              while(j>0 && l[i]!=l[j])
+                  j = p[j-1];
+              if(l[i]==l[j])
+                  j++;
+              p[i] = j;
+          }
+        return rev_s.substr(0,s.size()-p[l.size()-1])+s;
+    }
+};
