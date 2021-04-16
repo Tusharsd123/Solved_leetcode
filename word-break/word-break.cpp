dc4841b4c80f551080e39abddc332cb12e013be0
Tushar Diwakar <44468541+Tusharsd123@users.noreply.github.com>
2021-04-16 17:41:06 +0530

    Time: 8 ms (70.26%), Space: 9.5 MB (68.93%) - LeetHub

---
+++ word-break/word-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& dict) {
+        unordered_set<string> dicts;
+        for(auto i : dict)
+            dicts.insert(i);
+        if(dicts.size()==0)
+            return false;
+        vector<bool> dp(s.size()+1,false);
+        dp[0] = true;
+        for(int i = 1;i<=s.size();i++)
+        {
+            for(int j = i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(dicts.find(word)!=dicts.end())
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
