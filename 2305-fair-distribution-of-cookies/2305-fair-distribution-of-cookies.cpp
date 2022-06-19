38e762360b14b4dbe4c05b7f1f7acc00e008a1ca
Tushar Diwakar <44468541+Tusharsd123@users.noreply.github.com>
2022-06-19 17:40:21 +0530

    Time: 1235 ms (30.67%), Space: 6.9 MB (21.00%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,28 @@
+// Backtracking Algo
+class Solution {
+public:
+    int ans = INT_MAX;
+    
+    void backtrack(int start, vector<int> &v,vector<int> &cookies,int k){
+        if(start==cookies.size()){
+            int maxm = INT_MIN;
+            for(int i = 0;i<k;i++)
+                maxm = max(maxm,v[i]);
+            ans = min(ans,maxm);
+            return;
+        }
+        
+        for(int i = 0;i<k;i++){
+            v[i] += cookies[start];
+            backtrack(start+1,v,cookies,k);
+            v[i] -= cookies[start];
+        }
+    }
+    
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n = cookies.size();
+        vector<int> v(k,0);
+        backtrack(0,v,cookies,k);
+        return ans;
+    }
+};
